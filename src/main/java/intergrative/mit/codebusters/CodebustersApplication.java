76a88914f22bff1e66fc51e9315540091332afa7
Nipuna Muansinghe <mnipunai7@gmail.com>
2021-07-02 14:31:46 +0530

    Initial commit

---
+++ src/main/java/intergrative/mit/codebusters/CodebustersApplication.java
@@ -0,0 +1,13 @@
+package intergrative.mit.codebusters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodebustersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodebustersApplication.class, args);
+	}
+
+}
