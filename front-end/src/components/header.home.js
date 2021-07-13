02b14bd6b02a612c05747300dc9e3e4b2e9c2e95
MalingaHerath <malingashenal@gmail.com>
2021-07-13 19:49:14 +0530

    13/07/21
    final

--- front-end/src/components/header.home.js
+++ front-end/src/components/header.home.js
@@ -2,21 +2,22 @@ import React, { Component } from 'react';
 import './../index.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-export default class Header extends Component {
+export default class TokenHeader extends Component {
     render() {
         return (
             <div>
                 <nav className="navbar navbar-expand-lg navbar-light fixed-top">
                     <div className="container">
-                        <Link className="navbar-brand" to={"/sign-in"}>Code Busters</Link>
+                        <Link className="navbar-brand" to={"/home"}>Code Busters</Link>
                         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
                             <ul className="navbar-nav ml-auto">
-                                <li className="nav-item">
-                                    <Link className="nav-link" to={"/sign-in"}>Log In</Link>
+                            <li className="nav-item">
+                                    <Link className="nav-link" to={"/"}>Toekn</Link> {/* methanat token eka pass krapn */}
                                 </li>
                                 <li className="nav-item">
-                                    <Link className="nav-link" to={"/sign-up"}>Sign Up</Link>
+                                    <Link className="nav-link" to={"/sign-in"}>Log out</Link>
                                 </li>
+                                
                             </ul>
                         </div>
                     </div>
--- front-end/src/components/login.component.js
+++ front-end/src/components/login.component.js
@@ -1,86 +1,78 @@
 import React, { Component } from "react";
+import Header from "./header";
+import './login.signin.css';
 
 export default class Login extends Component {
-  constructor() {
-    super();
 
-    this.state = {
-      email: "",
-      password: ""
-    };
+    constructor() {
+        super();
 
-    this.onChange = this.onChange.bind(this);
-    this.onAuth = this.onAuth.bind(this);
-  }
+        this.state = {
+            email : '',
+            password : '',
+        }
 
-  onChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
+        this.onChange=this.onChange.bind(this)
+        this.onAuth=this.onAuth.bind(this)
+    }
 
-  onAuth(e) {
-    e.preventDefault();
-  }
+    onChange(e){
+        this.setState({[e.target.name]:e.target.value})
+    }
 
-  render() {
-    return (
-      <div className="outer">
-        <div className="inner">
-          <div>
+    onAuth(e) {
+        e.preventDefault()
+        //write api code
+        const {email,password} = this.state
+
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Header/>
             <form>
-              <h3>LOG IN</h3>
 
-              <div className="form-group">
-                <label>Email</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder="Enter email"
-                  name="email"
-                  required
-                  value={this.state.email}
-                  onChange={this.onChange}
-                />
-              </div>
+                <h3>LOG IN</h3>
 
-              <div className="form-group">
-                <label>Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="Enter password"
-                  name="password"
-                  required
-                  value={this.state.password}
-                  onChange={this.onChange}
-                />
-              </div>
-
-              <div className="form-group">
-                <div className="custom-control custom-checkbox">
-                  <input
-                    type="checkbox"
-                    className="custom-control-input"
-                    id="customCheck1"
-                  />
-                  <label
-                    className="custom-control-label"
-                    htmlFor="customCheck1"
-                  >
-                    Remember me
-                  </label>
+                <div className="form-group">
+                    <label>Email</label>
+                    <input 
+                    type="email" 
+                    className="form-control" 
+                    placeholder="Enter email" 
+                    name="email" 
+                    required 
+                    value={this.state.email}
+                    onChange={this.onChange}/>
                 </div>
-              </div>
 
-              <button type="submit" className="btn btn-dark btn-lg btn-block">
-                Log In
-              </button>
-              <p className="forgot-password text-right">
-                Already have an account?<a href="/sign-up">Sign In?</a>
-              </p>
+                <div className="form-group">
+                    <label>Password</label>
+                    <input 
+                    type="password" 
+                    className="form-control" 
+                    placeholder="Enter password" 
+                    name="password" 
+                    required 
+                    value={this.state.password}
+                    onChange={this.onChange}/>
+                </div>
+
+                <div className="form-group">
+                    <div className="custom-control custom-checkbox">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
+                    </div>
+                </div>
+
+                <button type="submit" className="btn btn-dark btn-lg btn-block">Log In</button>
+                <p className="forgot-password text-right">
+                    Already have an account?<a href="/sign-up">Sign In?</a>
+                </p>
             </form>
-          </div>
-        </div>
-      </div>
-    );
-  }
+            </div>
+        );
+    }
 }
